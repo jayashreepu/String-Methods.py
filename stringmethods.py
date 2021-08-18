3f02bf64b3c8a806f9f607c98cbf8e3b39f81c9f
jayashreepu <88489428+jayashreepu@users.noreply.github.com>
2021-08-18 16:20:05 +0530

    Created Stringmethods.py

---
+++ stringmethods.py
@@ -0,0 +1,253 @@
+# txt = "hello, My name is vijayashree."
+# x = txt.capitalize()
+# print (x)
+
+# txt = "hi, may i know your name ?"
+# x = txt.capitalize()
+# print (x)
+
+# txt = "excuse me, is this your seat ?"
+# x = txt.capitalize()
+# print (x)
+
+# txt = "Hi, My Name Is Vijayashree!"
+# x = txt.casefold()
+# print(x)
+
+# txt = "Hello, How are you ?"
+# x = txt.casefold()
+# print(x)
+
+# txt = "Excuse me, Is this your seat ?"
+# x = txt.casefold()
+# print(x)
+
+# txt = "Vijayashree Enterprises"
+# x = txt.center(65)
+# print(x)
+
+# txt = "Purushotham Enterprises"
+# x = txt.center(63)
+# print(x)
+
+# txt = "Nalliah Enterprises"
+# x = txt.center(66)
+# print(x)
+
+# txt = "I love apples, apples are my favorite fruit"
+# x = txt.count("apple")
+# print(x)
+
+# txt = "Banana is my favorite fruit"
+# x = txt.count("Apple")
+# print(x)
+
+# txt = "Apple is red in colour"
+# x = txt.count("blue")
+# print(x)
+
+# txt = "My name is Vijayashree"
+# x = txt.encode()
+# print(x)
+
+# txt = "I'm 18 years old"
+# x = txt.encode()
+# print(x)
+
+# txt = "I live in Bellandur"
+# x = txt.encode()
+# print(x)
+
+# txt = "Hello, welcome to my world."
+# x = txt.endswith(".")
+# print(x)
+
+# txt = "Hi, My Name is Vijayashree!"
+# x = txt.endswith("?")
+# print(x)
+
+# txt = "Hello, How are you?"
+# x = txt.endswith("?")
+# print(x)
+
+# txt = "H\te\tl\tl\to"
+# x = txt.expandtabs(2)
+# print(x)
+
+# txt = "H\te\ty!"
+# x = txt.expandtabs(3)
+# print(x)
+
+# txt = "H\ta\ti\ti!"
+# x = txt.expandtabs(4)
+# print(x)
+
+# txt = "Hello, welcome to my world."
+# x = txt.find("to")
+# print(x)
+
+# txt = "Hi, how are you?"
+# x = txt.find("are")
+# print(x)
+
+# txt = "Where are you from?"
+# x = txt.find("you")
+# print(x)
+
+# txt = "For only {price:.2f} dollars!"
+# print(txt.format(price = 49))
+
+# txt = "For only {price:.2f} dollars!"
+# print(txt.format(price = 69))
+
+# txt = "For only {price:.2f} dollars!"
+# print(txt.format(price = 99))
+
+# txt = "Hello, welcome to my world."
+# x = txt.index("welcome")
+# print(x)
+
+# txt = "Hi, How are you?"
+# x = txt.index("How")
+# print(x)
+
+# txt = "Where are you from?"
+# x = txt.index("from")
+# print(x)
+
+# txt = "Company12"
+# x = txt.isalnum()
+# print(x)
+
+# txt = "CompanyX"
+# x = txt.isalpha()
+# print(x)
+
+# txt = "Company123"
+# x = txt.isascii()
+# print(x)
+
+# txt = "\u0033" #unicode for 3
+# x = txt.isdecimal()
+# print(x)
+
+# txt = "50800"
+# x = txt.isdigit()
+# print(x)
+
+# txt = "Demo"
+# x = txt.isidentifier()
+# print(x)
+
+# txt = "hello world!"
+# x = txt.islower()
+# print(x)
+
+# txt = "565543"
+# x = txt.isnumeric()
+# print(x)
+
+# txt = "Hello! Are you #1?"
+# x = txt.isprintable()
+# print(x)
+
+# txt = "  "
+# x = txt.isspace()
+# print(x)
+
+# txt = "Hello, And Welcome To My Wold!"
+# x = txt.istitle()
+# print(x)
+
+# txt = "THIS IS NOW!"
+# x = txt.isupper()
+# print(x)
+
+# myTuple = ("John", "Peter", "Vicky")
+# x = "#".join(myTuple)
+# print(x)
+
+# txt = "watermelon"
+# x = txt.ljust(20)
+# print(x, is my favorite fruit.")
+
+# txt = "Hello my FRIENDS"
+# x = txt.lower()
+# print(x)
+
+# txt = "    watermelon    "
+# x = txt.lstrip()
+# print("of all fruits", x, "is my favorite")
+
+# txt = "Hello Pam!"
+# mytable = txt.marketrans("P","S")
+# print(txt.translate(mytable))
+
+# txt = "I could eat bananas all day"
+# x = txt.partition("bananas")
+# print(x)
+
+# txt = "I like bananas"
+# x = txt.replace("bananas", "watermelons")
+# print(x)
+
+# txt = "Mi casa, su casa."
+# x = txt.rfind("casa")
+# print(x)
+
+# txt = "Jai Hindh, Jai Karnataka."
+# x = txt.rindex("Karnataka")
+# print(x)
+
+# txt = "My Dad"
+# x = txt.rjust(20)
+# print(x, "Is My Hero.")
+
+# txt = "I could eat bananas all day, bananas are my favorite fruit"
+# x = txt.rpartition("bananas")
+# print(x)
+
+# txt = "Teena, Vijayashree, Yashu"
+# x = txt.rsplit(", ")
+# print(x)
+
+# txt = "    watermelon    "
+# x = txt.rstrip()
+# print("of all fruits", x, "is my favorite")
+
+# txt = "welcome to the jungle"
+# x = txt.split()
+# print(x)
+
+# txt = "Thank you for the music\nWelcome to the music world"
+# x = txt.splitlines()
+# print(x)
+
+# txt = "Hello, welcome to my world."
+# x = txt.startswith("Hello")
+# print(x)
+
+# txt = "    watermelon    "
+# x = txt.strip()
+# print("of all fruits", x, "is my favorite")
+
+# txt = "Hello My Name Is Vijayashree"
+# x = txt.swapcase()
+# print(x)
+
+# txt = "Welcome to my world"
+# x = txt.title()
+# print(x)
+
+#use a dictionary with ascii codes to replace 83 (S) with 80 (P):
+#mydict = {83: 80}
+# txt = "Hello Sam!"
+# print(txt.translate(mydict))
+
+# txt = "Hello my friends"
+# x = txt.upper()
+# print(x)
+
+# txt = "50"
+# x = txt.zfill(10)
+# print(x)
